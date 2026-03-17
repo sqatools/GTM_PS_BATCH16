a35bd3c1f5d97b9e116d74985f7c3be28a296f5d
drushabh15-dev <drushabh15@gmail.com>
2026-03-17 15:49:06 +0530

    adding code

--- Rushabh/Python_Program/Python_variable.py
+++ Rushabh/Python_Program/Python_variable.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 print("Hello World")
 a = 10
 # a : variable
@@ -7,8 +10,71 @@ b = 30
 c = 10
 
 print("value of a :", a, id(a))
-# value of a : 10 140705768174792
+# value of a : 10 140704539899080
 print("value of b :", b, id(b))
 # value of b : 30 140705768175432
 print("value of c :", c, id(c))
-# value of c : 10 140705768174792
+# value of c : 10 140704539899080
+
+
+x , y , z = 50, 60, 70
+print("value of x :", x) # 50   
+print("value of y :", y)# 60
+print("value of z :", z)# 70
+
+p = q = r = 500
+print("value of P:", p) # 500    
+print("value of q:", q) # 500
+print("value of r:", r) # 500
+print("value of p, q, r :", p, q, r)
+
+
+name = "Rahul"
+Name = "Mohit"
+NAME = "Raghav"
+namE = "Ravi"
+NamE = "Rohan"
+print(name, Name, NAME, namE, NamE)
+
+
+var1 = 40
+var_123_a = 50
+_pqr_hello = "Python Programming"
+print("value of var1:", var1)
+print("value of var_123_a:", var_123_a)
+print("value of _pqr_hello:", _pqr_hello)
+
+
+num1 = 10
+num2 = 20   
+
+addition = num1 + num2
+print("Addition of num1 and num2:", addition)   
+
+subtraction = num1 - num2
+print("Subtraction of num1 and num2:", subtraction)
+
+multiplication = num1 * num2
+print("Multiplication of num1 and num2:", multiplication)
+
+division = num1 / num2
+print("Division of num1 and num2:", division)   
+
+modulus = num1 % num2
+print("Modulus of num1 and num2:", modulus)
+
+square_num1 = num1 ** 2
+print("Square of num1:", square_num1)
+
+square_num2 = num2 ** 2
+print("Square of num2:", square_num2)   
+
+cube_num1 = num1 ** 3
+print("Cube of num1:", cube_num1)
+
+cube_num2 = num2 ** 3
+print("Cube of num2:", cube_num2)   
+
+floor_division = num1 // num2
+print("Floor division of num1 and num2:", floor_division)   
+
