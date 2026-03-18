24d9631cb1fd2fc3f095a8e32b16fd2d89073291
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-03-19 00:26:36 +0530

    added few code

---
+++ Ramesh/PythonPrograming/Variable/pyPrac_IfElse.py
@@ -0,0 +1,270 @@
+# 1
+
+
+i = 201
+
+if i%2 == 0:
+    print ("The number is even")
+else:
+    print ("The number is odd")
+
+
+
+
+
+# QUE NO. 2  HAVE TO UNDERSTAND 
+
+# 3
+a = 39
+
+if a < 40:
+    print("The result is FAIL")
+elif a >= 40 and a <=50:
+    print("The result is Grade C")
+elif a >= 51 and a <=60:
+    print("The result is Grade B")
+elif a >= 61 and a <= 70:
+    print("The result is Grade A")
+elif a >= 71 and a <= 80:
+    print("The result is Grade A+")
+elif a >= 81 and a <= 90:
+    print ("The result is Grade A++")
+elif a >= 91 and a <= 100:
+    print ("The result is Grade EXCELLENT")
+else :
+    print ("The result is Invalid")
+
+
+
+
+##############################
+
+
+# 4
+b = 15
+
+if b % 3 == 0 and b % 5 == 0:
+    print ("The number is divided by the given number")
+else:
+    print ("The number is not divided by the given number ")
+
+
+
+#############################
+
+
+
+# 5
+c = 22
+
+if c % 11 == 0:
+    print ("""The number is divided by the given number
+    Square of the number is : """, c**2)
+else:
+    print("The number is not divided by the number")
+
+
+# 6 i need to get on it 
+
+
+
+############################
+
+
+
+# 7
+
+
+
+x = 11
+
+if x % 2 == 0:
+    print ("The give number is EVEN")
+else:
+    print("The given number is ODD")
+
+
+
+############################
+
+
+
+# 11
+
+f = 6
+
+if f % 2 == 0 and f % 3 ==0:
+    print("""The number is divided by the given number 
+    cube of the number is : """, f **3)
+else:
+    print("The number is not divided by thr given number")
+
+
+
+################################
+
+
+
+# 12
+
+
+interview = 80
+
+if interview >=50 and interview <=100:
+    print("Interview Section cleared")
+else:
+    print("Interview Section not cleared")
+
+
+
+
+
+
+##################################
+
+
+
+
+
+# 13
+
+list1 = [1, 2, 3, 4, 5]
+
+h = 56
+
+if h in list1:
+    print("The number is present in the list")
+else:
+    print("The number is not present in the list")
+
+
+
+
+    ############################################
+
+
+
+# 14
+
+
+j = 100
+k = 20 
+l = 30
+
+if j > k and j > l:
+    print("j is the largest number")
+elif k > j and k > l:
+    print ("k is the largest number ")
+else:
+    print("l is the largest number")
+
+
+
+
+#########################################
+
+
+
+
+# 15
+
+z = 26
+
+if z > 18:
+    print ("person is eligible for vote")
+else:
+    print("Person is not eligible for vote")
+
+
+
+
+
+
+########################################
+
+
+
+# 18 
+
+
+n = 45
+
+if n > 35:
+    print("The student is pass")
+else:
+    print("The student is fail") 
+
+
+
+###############################
+
+
+
+# 19 & 20
+
+q = 20
+
+if q % 2 == 0:
+    print ("The number is true")
+else:
+    print("The number is false")
+
+
+
+#######################################
+
+
+
+# 22
+
+
+m = 10
+w = 20
+
+if m > w:
+    print ("m is greater than w")
+else:
+    print("w is greater than m")
+
+
+
+##################################
+
+
+# 25
+
+e = 10
+
+if type(e) == int:
+    print("The given value is integer")
+else:
+    print("The given value is not integer")
+
+
+###################################
+
+
+# 26
+
+u = 1.2
+
+if type(u) == float:
+    print("The given value is float")
+else:
+    print("The given value is not float")
+
+
+
+
+###################################
+
+
+
+# 27
+
+v = "Hello"
+
+if type(v) == str:
+    print("The given value is string")
+else:
+    print("The given value is not string")
