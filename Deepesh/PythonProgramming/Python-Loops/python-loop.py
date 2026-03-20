d85f66041dc1eb9b56a7caae843d8553f57a668b
sqatools <sqatools.automation@gmail.com>
2026-03-20 09:46:43 +0530

    added code

--- Deepesh/PythonProgramming/Python-Loops/python-loop.py
+++ Deepesh/PythonProgramming/Python-Loops/python-loop.py
@@ -97,3 +97,91 @@ list1 = [6, 7, 9, 23, 45, 12, 68, 24, 18]
 
 # write a program to square all the value which are divisible by 5
 list2 = [5, 10, 7, 23, 12, 5, 7, 35, 45]
+
+print("#"*50)
+#####################################
+# Nested for loop :  In nested for loop, for one single value of outer loop, executes entire inner loop will execute
+
+for i in range(1, 5):  # i = 1, 2
+    print("Address i :", i) # 1
+    for j in range(1, 4): # 1, 2, 3
+        print("Package j :", j)
+
+    print("_"*20)
+
+
+#####################################
+# program to check the prime number.
+
+num = 11
+prime = True
+for i in range(2, num):
+    print(i)
+    if num%i == 0:
+        prime = False
+        break
+
+if prime == True:
+    print("This is Prime Number:", num)
+else:
+    print("This is not Prime Number:", num)
+
+
+#####################################
+print("#"*50)
+for num in range(2, 100):
+    prime = True
+    for i in range(2, num):
+        #print(i)
+        if num%i == 0:
+            prime = False
+            break
+
+    if prime == True:
+        print(num)
+
+#####################################
+print("#"*50)
+
+# while loop
+n = 1
+while(n<= 10):
+    print(n)
+    n += 1
+
+
+# infinite loop
+n = 1
+while(True):
+    print(n)
+    #n += 1
+    n = n + 1
+    if n == 1000:
+        break
+
+
+######################################
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+
+# print("hello", end=" ")
+# print("Python", end=" ")
+
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end=" ")
+
+    print()
+
+for i in range(5 , 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+
+    print()
+
