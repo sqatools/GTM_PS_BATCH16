7e9a114b1386b41defb499f8cd9b4fb7bfb85928
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-03-31 00:11:46 +0530

    new code added

---
+++ Ramesh/PythonPrograming/Variable/PRACTICE_Funtion.py
@@ -0,0 +1,16 @@
+# 1. Python function program to add two numbers.
+
+def add(a , b):
+    print(a + b)
+
+add(1, 2)
+
+
+# 2.  Python function program to print the input string 10 times.
+
+
+def str():
+    print("  Hello world " * 10)
+
+
+str()
