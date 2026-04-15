7303f1410ded5f0ceb655dc9d16470e585a6ecae
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-04-16 01:52:19 +0530

    new code adde

--- Ramesh/PythonSelenium/Basic/selenium_locators.py
+++ Ramesh/PythonSelenium/Basic/selenium_locators.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 # global variable to hold the driver isinstance
 driver = None
@@ -18,7 +19,48 @@ def launch_website_and_verify():
 
 def selenium_locators():
     # ID locator
-    driver.find_element(By.ID, "form_first_name_7").send_keys("Ramesh")
-    driver.find_element(By.ID, "form_email_7").send_keys("ramesh@123.com")
+    driver.find_element(By.ID, "fromcity").send_keys("Assam")
+    driver.find_element(By.ID, "destcity").send_keys("Guwahati")
 
     # Name locator
+    driver.find_element(By.NAME, "departdate").send_keys("01/01/2026")
+    driver.find_element(By.NAME, "returndate").send_keys("03/01/2026")
+
+    # Class Name locator
+    heading = driver.find_element(By.CLASS_NAME, "entry-title").text
+    print("Heading of the page is :", heading)
+    
+    
+    # link text locator
+    driver.find_element(By.LINK_TEXT, "Home").click()
+    
+
+
+    # partial link text locator
+    driver.find_element(By.PARTIAL_LINK_TEXT, "String Programs").click()
+    time.sleep(5)
+
+# selenium methods to find element 
+
+def selenium_get_elements():
+    # get list of elements using find_elements method
+    username_fields = driver.find_elements(By.ID, "firstname")
+    username_fields[0].send_keys("Ramesh")
+    username_fields[1].send_keys("Kumar")
+    time.sleep(5)
+
+    # get all links on the page
+    list_of_links = driver.find_elements(By.TAG_NAME, "a")
+    print("All links on the page:", len(list_of_links))
+    for link in list_of_links:
+        print("Link Text:", link.text, " - URL:", link.get_attribute("href"))
+
+    
+
+
+
+
+
+launch_website_and_verify()
+# selenium_get_elements()
+selenium_locators()
