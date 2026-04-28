cf51f906e6c8604a825883cfdb4869242950a0e1
sqatools <sqatools.automation@gmail.com>
2026-04-28 09:00:07 +0530

    added new code

---
+++ Deepesh/AutomationFramework/tests/dummywebsite/test_dummy_website_case.py
@@ -0,0 +1,18 @@
+import pytest
+import time
+from page_objects.dummy_page.dummy_page_class import DummyPage
+
+
+@pytest.mark.usefixtures("get_driver_class")
+class TestDummyWebsite:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.dp = DummyPage(self.driver)
+
+
+    def test_enter_source_dest_city(self):
+        self.dp.enter_source_and_destination("Mumbai", "Pune")
+        time.sleep(10)
+
+        
+        
