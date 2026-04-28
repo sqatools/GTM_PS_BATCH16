7b9f0de216f07887620fc49451894fe8b28a8dca
git-ekta1990 <ektagund10@gmail.com>
2026-04-29 01:13:35 +0530

    Adding files to git

--- Ekta/SQA_TOOLS_Practice/seondlargestnumber.py
+++ Ekta/SQA_TOOLS_Practice/seondlargestnumber.py
@@ -5,3 +5,11 @@ print("List elements ", list)
 list.sort()
 print("After sorting ", list)
 print("second largest number",list[-2])
+
+
+list1=[50,100,25,5,75,500]
+
+list1.sort(reverse=True)
+print("List1 in descending order ", list1)
+
+
