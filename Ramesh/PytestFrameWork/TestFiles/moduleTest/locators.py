897d3e01c59267b6b99433c7d5947eb75b1f9301
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-04-30 00:53:09 +0530

    new code add

--- Ramesh/PytestFrameWork/TestFiles/moduleTest/locators.py
+++ Ramesh/PytestFrameWork/TestFiles/moduleTest/locators.py
@@ -5,6 +5,14 @@ class DummyLocators:
     DestCityField = (By.ID, "destcity")
 
     DepartDate = (By.NAME, "departdate")
-    ReturnDate = (By.NAME, "retutndate")
+    ReturnDate = (By.NAME, "returndate")
 
     PageHeading = (By.CLASS_NAME, "entry-title")
+
+
+    male_radio_btn = (By.ID, "male")
+
+
+    passenger_dropdown = (By.ID, "admorepass")
+
+    contry_dropdown = (By.ID, "billing_country")
