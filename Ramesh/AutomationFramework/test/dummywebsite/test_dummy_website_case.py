062921ddb416505e296c6ddad4f867ea48f8d392
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-05-02 02:09:18 +0530

    added new code

--- Ramesh/AutomationFramework/test/dummywebsite/test_dummy_website_case.py
+++ Ramesh/AutomationFramework/test/dummywebsite/test_dummy_website_case.py
@@ -1,6 +1,7 @@
 import pytest
 import time
 from page_objects.dummy_page.dummy_page_class import DummyPage
+from utilities.utils import Utils
 
 
 @pytest.mark.usefixtures("get_driver_class")
@@ -9,8 +10,15 @@ class TestDummyWebsite:
     def setup(self):
         self.dp = DummyPage(self.driver)
 
+        self.util = Utils()
+        self.user_data = self.util.read_json_data("page_objects/dummy_page/user_data.json")
+
 
     def test_enter_source_dest_city(self):
-        self.dp.enter_source_anddestination("Mumbai", "Pune")
+        # self.dp.enter_source_anddestination("Mumbai", "Pune")
+        self.dp.enter_source_anddestination(self.user_data["fromCity"], self.user_data["destcity"])
         # import pdb; pdb.set_trace()
         time.sleep(10)
+
+
+    
