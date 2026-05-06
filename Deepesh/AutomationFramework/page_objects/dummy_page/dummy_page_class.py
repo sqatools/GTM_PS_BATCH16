91adec72a2f11be8a555fa160ca761d2c893928b
sqatools <sqatools.automation@gmail.com>
2026-05-06 09:06:37 +0530

    addd logs in report

--- Deepesh/AutomationFramework/page_objects/dummy_page/dummy_page_class.py
+++ Deepesh/AutomationFramework/page_objects/dummy_page/dummy_page_class.py
@@ -7,7 +7,24 @@ class DummyPage(SeleniumBase):
       self.dl = DummyLocators()
 
     def enter_source_and_destination(self, fromcity, destcity):
+       self.log.info("Execute Method : enter_source_and_destination")
        self.enter_text(self.dl.fromCityField, fromcity)
        self.enter_text(self.dl.DestCityField, destcity)
+
+    def select_gender(self, gender):
+        if gender.lower() == "male":
+          self.click_element(self.dl.male_radio_btn)
+        elif gender.lower() == "female":
+          self.click_element(self.dl.female_radio_btn)
+
+
+    def select_number_of_passengers(self, value):
+      self.select_dropdown_value(self.dl.passenger_dropdown, value)
+
+
+    def select_country(self, country_name):
+       self.select_dropdown_value(self.dl.contry_dropdown, country_name)
+           
+
 
     
