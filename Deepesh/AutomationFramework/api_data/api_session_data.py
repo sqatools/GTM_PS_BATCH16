91cc7d8012d97807cdf282c0e357199ba8969c76
sqatools <sqatools.automation@gmail.com>
2026-05-12 09:10:23 +0530

    added

--- Deepesh/AutomationFramework/api_data/api_session_data.py
+++ Deepesh/AutomationFramework/api_data/api_session_data.py
@@ -4,3 +4,14 @@ go_rest = "https://gorest.co.in/"
 get_all_users_url = f"{go_rest}/public/v2/users"
 go_rest_headers = {"Authorization": "Bearer ad6b849f911b220a37b1218280ce88bfa087d8c8f82bee717fd58a47d40f1277",
                    "Content-Type" : "application/json"}
+
+post_request_data = {
+        "name": "Apple MacBook Pro 200",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+        }
+headers = {"Content-Type" : "application/json"}
