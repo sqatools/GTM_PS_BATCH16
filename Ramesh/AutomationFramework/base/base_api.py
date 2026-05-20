d5763d80de909836c516bcf577d0ec82dd318e0e
rames15 <RAMESHKASHYAP1880@GMAIL.COM>
2026-05-21 01:08:58 +0530

    added new file

---
+++ Ramesh/AutomationFramework/base/base_api.py
@@ -0,0 +1,71 @@
+import requests
+import logging
+
+class BaseAPI:
+    def __init__(self):
+        self.log = logging.getLogger(__name__)
+
+    def get_method(self, url, header=None, request_body=None):
+        header = header if header else {}
+        request_body = request_body if request_body else {}
+        response = requests.request("GET", url, headers=header, data=request_body)
+        self.log.info("Method Name : GET")
+        self.log.info(f"URL :{url}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"Request body : {request_body}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"response : {response.json()}")
+        self.log.info(f"status code : {response.status_code}")
+        return response
+    
+    def post_method(self, url, header=None, request_body=None):
+        header = header if header else {}
+        request_body = request_body if request_body else {}
+        response = request.request("POST", url, headers=headers, data=request_body)
+        self.log.info("Method Name : POST")
+        self.log.info(f"URL :{url}")
+        self.log.info(f"Headers : {headers}")
+        self.log.info(f"Request body : {request_body}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"response : {response.json()}")
+        self.log.info(f"status code : {response.status_code}")
+        return response
+    
+    def put_method(self, url, header=None, request_body=None):
+        header = header if header else {}
+        request_body = request_body if request_body else {}
+        response = request.request("PUT", url, headers=headers, data=request_body)
+        self.log.info("Method Name : PUT")
+        self.log.info(f"URL :{url}")
+        self.log.info(f"Headers : {headers}")
+        self.log.info(f"Request body : {request_body}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"response : {response.json()}")
+        self.log.info(f"status code : {response.status_code}")
+        return response
+    
+    def patch_method(self, url, header=None, request_body=None):
+        header = header if header else {}
+        request_body = request_body if request_body else {}
+        response = request.request("PATCH", url, headers=headers, data=request_body)
+        self.log.info("Method Name : PATCH")
+        self.log.info(f"URL :{url}")
+        self.log.info(f"Headers : {headers}")
+        self.log.info(f"Request body : {request_body}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"response : {response.json()}")
+        self.log.info(f"status code : {response.status_code}")
+        return response
+    
+    def delete_method(self, url, header=None, request_body=None):
+        header = header if header else {}
+        request_body = request_body if request_body else {}
+        response = request.request("DELETE", url, headers=headers, data=request_body)
+        self.log.info("Method Name : DELETE")
+        self.log.info(f"URL :{url}")
+        self.log.info(f"Headers : {headers}")
+        self.log.info(f"Request body : {request_body}")
+        self.log.info(f"Headers : {header}")
+        self.log.info(f"response : {response.json()}")
+        self.log.info(f"status code : {response.status_code}")
+        return response
