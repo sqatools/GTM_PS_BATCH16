039aa4b51bdf35c67b232f9e49fb61ee65ce62dd
sqatools <sqatools.automation@gmail.com>
2026-05-22 09:12:15 +0530

    added new file

--- Deepesh/AutomationFramework/tests/conftest.py
+++ Deepesh/AutomationFramework/tests/conftest.py
@@ -2,17 +2,19 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-headless = True
+headless = False
 
 
 @pytest.fixture(scope="class")
 def get_driver_class(request):
     print("Launching Browser")
     opt = Options()
-    opt.add_argument("--headless")
+    if headless:
+        opt.add_argument("--headless")
     driver = webdriver.Chrome(options=opt)
     driver.get("https://sqatools.in/dummy-booking-website/")
     driver.maximize_window()
     driver.implicitly_wait(10)
     request.cls.driver = driver
     yield driver
+    driver.quit()
