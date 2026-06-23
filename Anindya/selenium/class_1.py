b5833bdf091a07c30c55357849065dc1e20c2d5b
Anindya420 <anindyapl389@gmail.com>
2026-06-23 13:08:48 +0530

    test

--- Anindya/selenium/class_1.py
+++ Anindya/selenium/class_1.py
@@ -1,18 +1,77 @@
+import selenium
+
 from selenium import webdriver
-import time
+
 from selenium.webdriver.common.by import By
 
-def func():
+import time
+
+driver = None
+def launch():
+    global driver 
+
     driver = webdriver.Chrome()
     driver.get("https://sqatools.in/dummy-booking-website/")
 
     driver.maximize_window()
 
-    driver.find_element(By.ID,"fromcity").send_keys("bangalore")
-    driver.find_element(By.ID,"destcity").send_keys("Mumbai")
-    time.sleep(3)
+    assert driver.current_url == "https://sqatools.in/dummy-booking-website/"
+    print(driver.title)
 
-    driver.close()
+def locator():
+    # ID locator
+    driver.find_element(By.ID,"fromcity").send_keys("Mumbai")
+    driver.find_element(By.ID,"destcity").send_keys('Chennai')
 
-func()
+    # Name locator
+    driver.find_element(By.NAME,"departdate").send_keys("10/10/2026")
+    driver.find_element(By.NAME,"returndate").send_keys("23/10/2026")
+
+    # class locator
+    b = driver.find_element(By.CLASS_NAME,"entry-title").text
+    print(b)
+
     
+   
+    # link text
+
+    # driver.find_element(By.ID,"menu-menu_main").driver.find_element(By.LINK_TEXT,"Home").click()
+
+    # time.sleep(15)
+
+    # driver.find_element(By.XPATH,"//div[text()='Close']").click()
+
+    # # time.sleep(15)
+
+    # # partiAL link text
+
+    # driver.find_element(By.PARTIAL_LINK_TEXT,"String Programs").click()
+    # time.sleep(5)
+    # driver.close()
+
+def multiple_element():
+        w = driver.find_elements(By.ID,"firstname")
+        print("The value is w is ", len(w))
+        w[0].send_keys("This")
+        w[1].send_keys("That")
+
+        time.sleep(2)
+
+        v = driver.find_elements(By.TAG_NAME,"a")
+        print("Total lenght of link",len(v))
+
+        for item in v :
+            print("link text is", item.text, "and the url is ", item.get_attribute("href"))
+
+        time.sleep(10)
+        driver.close()    
+
+
+
+
+
+launch()
+locator()
+
+
+multiple_element()
