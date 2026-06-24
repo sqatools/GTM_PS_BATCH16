7ccd22668695c3b59e23a316f7aa657da53b27ac
Anindya420 <anindyapl389@gmail.com>
2026-06-24 08:54:16 +0530

    test

--- Anindya/selenium/class_1.py
+++ Anindya/selenium/class_1.py
@@ -20,16 +20,49 @@ def launch():
 
 def locator():
     # ID locator
-    driver.find_element(By.ID,"fromcity").send_keys("Mumbai")
-    driver.find_element(By.ID,"destcity").send_keys('Chennai')
+    # driver.find_element(By.ID,"fromcity").send_keys("Mumbai")
+    # driver.find_element(By.ID,"destcity").send_keys('Chennai')
 
-    # Name locator
-    driver.find_element(By.NAME,"departdate").send_keys("10/10/2026")
-    driver.find_element(By.NAME,"returndate").send_keys("23/10/2026")
+    # Xpath locator
 
-    # class locator
-    b = driver.find_element(By.CLASS_NAME,"entry-title").text
+    # driver.find_element(By.XPATH,"//*[@name='fromcity']").send_keys("Anindya")
+    a = driver.find_element(By.XPATH,"//h2[text()='Passenger Details']").text
+    print(a)
+    driver.find_element(By.XPATH,"//input[contains(@name,'mcity')]").send_keys("Abhijit")
+
+    
+
+    b = driver.find_element(By.XPATH,"//h2[contains(text(),'Delivery Option')]").text
     print(b)
+    
+
+    c = driver.find_element(By.XPATH,"//span[text()='Dummy return ticket – $300 ']/parent::li").text
+    print(c)
+
+    d = driver.find_element(By.XPATH,"//span[text()='Dummy ticket for visa application – $200 ']/ancestor::div[@align='left']").text
+    print(d)
+
+    driver.find_element(By.XPATH,"//input[@name='fromcity']/following::input[@id='billing_name']").send_keys("Anindya")
+
+    driver.find_element(By.XPATH,"//input[@name='fromcity']/preceding::input[@id='male']").click()
+    time.sleep(5)
+
+    w =driver.find_element(By.XPATH,"//td[text()='Kolkata']/following-sibling::td[text()='5000']").text
+    print(w) 
+    g = driver.find_element(By.XPATH,"//td[text()='Kolkata']/preceding-sibling::td[text()='6004']").text
+    print(g)
+    driver.find_element(By.XPATH,"//td[text()='Kolkata']/preceding-sibling::td/input[@value='checkbox']").click()
+    time.sleep(5)
+    driver.close()
+
+
+    # # Name locator
+    # driver.find_element(By.NAME,"departdate").send_keys("10/10/2026")
+    # driver.find_element(By.NAME,"returndate").send_keys("23/10/2026")
+
+    # # class locator
+    # b = driver.find_element(By.CLASS_NAME,"entry-title").text
+    # print(b)
 
     
    
@@ -49,22 +82,22 @@ def locator():
     # time.sleep(5)
     # driver.close()
 
-def multiple_element():
-        w = driver.find_elements(By.ID,"firstname")
-        print("The value is w is ", len(w))
-        w[0].send_keys("This")
-        w[1].send_keys("That")
+# def multiple_element():
+#         w = driver.find_elements(By.ID,"firstname")
+#         print("The value is w is ", len(w))
+#         w[0].send_keys("This")
+#         w[1].send_keys("That")
 
-        time.sleep(2)
+#         time.sleep(2)
 
-        v = driver.find_elements(By.TAG_NAME,"a")
-        print("Total lenght of link",len(v))
+#         v = driver.find_elements(By.TAG_NAME,"a")
+#         print("Total lenght of link",len(v))
 
-        for item in v :
-            print("link text is", item.text, "and the url is ", item.get_attribute("href"))
+#         for item in v :
+#             print("link text is", item.text, "and the url is ", item.get_attribute("href"))
 
-        time.sleep(10)
-        driver.close()    
+#         time.sleep(10)
+#         driver.close()    
 
 
 
@@ -74,4 +107,4 @@ launch()
 locator()
 
 
-multiple_element()
+# multiple_element()
