25add6cebbab486f5fbc46d29ba30016120e4b1a
Anindya420 <anindyapl389@gmail.com>
2026-07-05 16:50:37 +0530

    test

---
+++ Anindya/Python_progtamming/Python_OOPs/polimorchisom.py
@@ -0,0 +1,35 @@
+class Father:
+    def __init__(self,fname, fcar):
+        self.fname = fname
+        self.fcar = fcar
+
+    def show_father_details(self):
+        print(f'the father name is {self.fname} and the car is {self.fcar}')
+
+    def func(self):
+        print("Welcome to Father class")    
+
+
+class Child(Father):
+    def __init__(self,son,fname,fcar):
+        super().__init__(fname,fcar)
+        self.son = son
+
+
+    def show_son_details(self) :
+        print(f'the son name is {self.son}')
+
+    def all_details(self):
+        self.show_father_details()
+        self.show_son_details()
+
+    def func(self):
+        print("Welcome to Son class")    
+
+        
+
+obj = Child("Anindya","Arun","Scoppio")
+
+obj.all_details()
+
+obj.func()
